cac130e53d34da4247fe3a65d7f3456d63baea17
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-07-07 09:00:09 -0300

    a

---
+++ ex0002/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mais é oque mo fih??</title>
+</head>
+<body>
+
+        <?php 
+        
+        ?>
+
+</body>
+</html>
