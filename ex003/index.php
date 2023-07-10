f7b7c92365d20a8cac8a7cc6ace5b5114e7b456e
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-07-10 10:16:41 -0300

    variaveis e constantes

---
+++ ex003/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testes de variaveis e constantes</title>
+</head>
+<body>
+    <?php  
+    $nome = "Guilherme";
+    $sobrenome = "Souza Muller";
+    const PAIS = "Brasil";
+
+    echo "Muito prazer, $nome $sobrenome. Você mora no " . PAIS . "?";
+    ?>
+</body>
+</html>
