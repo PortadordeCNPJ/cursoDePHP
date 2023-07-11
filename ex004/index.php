1ccdd0b76e4a1eeabcb2e62892c84f72533f6af9
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-07-11 08:24:41 -0300

    att

--- ex004/index.php
+++ ex004/index.php
@@ -9,27 +9,27 @@
     <h1>Teste de tipo Primitivos</h1>
 
     <?php 
-    // // 0x = hexadecimal, 0b = binário, 0 = Octal.
-    // $num = 300;
-    // echo "O valor da variável é $num ";
+    // 0x = hexadecimal, 0b = binário, 0 = Octal.
+    $num = 300;
+    echo "O valor da variável é $num ";
 
-    // //Joga resultados sobre a variavel selecionada na tela.
-    // $v = 34;
-    // var_dump($v);
+    //Joga resultados sobre a variavel selecionada na tela.
+    $v = 34;
+    var_dump($v);
 
-    // $num_1 = 3e2; //3 x 10(2)
-    // echo " O valor é $num_1 ";
-    // var_dump($num_1);
+    $num_1 = 3e2; //3 x 10(2)
+    echo " O valor é $num_1 ";
+    var_dump($num_1);
 
-    // $num_2 = (int) " 950 ";
-    // var_dump($num_2);
+    $num_2 = (int) " 950 ";
+    var_dump($num_2);
 
-    // $casado = true;
-    // echo "O valor para casado é $casado.";
+    $casado = true;
+    echo "O valor para casado é $casado.";
 
     //Struct em PHP
-    // $vet = [6, 2, 5, "Maria", 3, false];
-    // var_dump($vet);
+    $vet = [6, 2, 5, "Maria", 3, false];
+    var_dump($vet);
 
     class Pessoa {
         private string $nome;
